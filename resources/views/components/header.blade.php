8a657327746a2113f14b2ac82b87345e9e41a925
sedy <sedynah.07@gmail.com>
2026-05-19 15:37:03 +0300

    change header into component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,17 @@
+<div class="flex items-center justify-between">
+    <div class="pt-2">
+        <h1 class="text-white text-2xl">S <span class="text-gray-400">D</span></h1>
+    </div>
+    <div class="hidden md:flex space-x-12">
+        <a href="#" class="text-white hover:text-darkGrayishBlue">Expériences</a>
+        <a href="#" class="text-white hover:text-darkGrayishBlue">Projets</a>
+        <a href="#" class="text-white hover:text-darkGrayishBlue">Services</a>
+        <a href="#" class="text-white hover:text-darkGrayishBlue">Contact</a>
+    </div>
+</div>
+<div aria-hidden="true"
+     class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+    <div
+        style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+        class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#CD8E65] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"></div>
+</div>
